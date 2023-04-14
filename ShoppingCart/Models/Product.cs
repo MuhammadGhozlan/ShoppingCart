94365e251766218caa222b6f9a790c0cd15217bc
Muhammad GHozlan <Muhammad.Ghozlan@yahoo.com>
2023-04-14 07:21:22 -0500

    commit

--- ShoppingCart/Models/Product.cs
+++ ShoppingCart/Models/Product.cs
@@ -8,25 +8,29 @@ namespace ShoppingCart.Models
     {
         [Key]
         public long Id { get; set; }
+
         [Required(ErrorMessage = "Please Enter a Value")]
         public string Name { get; set; }
+
         public string? Slug { get; set; }
+
         [Required, MinLength(4, ErrorMessage = "Minimum Length is 2")]
         public string Description { get; set; }
-         [Required]
+
+        [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Please Enter a Value")]
         [Column(TypeName = "decimal(8,2)")]
         public decimal Price { get; set; }
+
         [Required, Range(1, int.MaxValue, ErrorMessage = "You Must Choose a Category")]
-
-
         public long CategoryId { get; set; }
-        public string? Image { get; set; }
         public Category? Category { get; set; }
+        public string Image { get; set; } = "noimage.png";
+        
 
         [NotMapped]
         [FileExtension]
-        public IFormFile ImageUpload { get; set; }
+        public IFormFile? ImageUpload { get; set; }
     }
 }
 
